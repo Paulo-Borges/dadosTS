9b7ac64a50761c94af0729e826009f62b15d97f8
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-10-05 18:49:19 -0300

    Update getDate

--- src/Context/DataContext.tsx
+++ src/Context/DataContext.tsx
@@ -30,16 +30,21 @@ export const useData = () => {
   return context;
 };
 
-function getDate() {
+function getDate(n: number) {
+  // data do dia de hoje
   const date = new Date();
-  console.log(date);
+  date.setDate(date.getDate() - n);
+  const dd = String(date.getDate()).padStart(2, "0");
+  const mm = String(date.getMonth() + 1).padStart(2, "0");
+  const yyyy = date.getFullYear();
+  return `${yyyy}-${mm}-${dd}`;
 }
-getDate();
+// getDate();
 
 // Criando o provedor, onde envolve toda aplicação
 export const DataContextProvider = ({ children }: React.PropsWithChildren) => {
-  const [inicio, setInicio] = React.useState("");
-  const [final, setFinal] = React.useState("");
+  const [inicio, setInicio] = React.useState(getDate(14));
+  const [final, setFinal] = React.useState(getDate(0));
 
   const { data, loading, error } = useFetch<IVenda[]>(
     `https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`
